dd4ca67ffd2037992d61358c2d4fe6091eaae29e
Ken Cluff <KCluff50@gmail.com>
2019-03-28 22:33:29 -0600

    Initial Commit

---
+++ LightDark/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LightDark
+//
+//  Created by Kenneth Cluff on 3/28/19.
+//  Copyright © 2019 Kenneth Cluff. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
